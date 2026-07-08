794cbbb6eec7abf0628caa7f8b24d68696ce251c
Dhruva <136962778+dhruva-vadharodiya@users.noreply.github.com>
2026-07-08 21:37:22 +0530

    Add production deployment script

---
+++ prod.py
@@ -0,0 +1,12 @@
+print("=" * 40)
+print("Production Stage")
+print("=" * 40)
+
+print("Preparing Production Deployment...")
+print("Creating Release Package...")
+print("Deploying Application...")
+print("Verifying Deployment...")
+
+print("\nApplication deployed successfully.")
+
+print("Status : SUCCESS")
